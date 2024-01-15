b61e8dc06e869718f3554c5a7c79792a13aff973
Chico-Estudioso <mauroserrano01@educarex.es>
2024-01-15 09:19:55 +0000

    15/1

---
+++ database/migrations/2024_01_15_084522_create_pedido__productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedido_productos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('cantidad');
+            $table->float('precioU');
+            $table->foreignId('pedido_id')->constrained()->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->integer('producto');
+            $table->foreign('producto')->references('id')->on('productos')->constrained()
+                ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedido_productos');
+    }
+};
